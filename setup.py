2ae032a765d5ad04596021d9731666e5385163f0
adina-calin <64332698+adina-calin@users.noreply.github.com>
2020-07-07 10:20:41 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name="ev_contracte",
+    version="0.1",
+    packages=find_packages('src/ev_contracte'),
+    package_dir={'':'src/ev_contracte'}
+)
