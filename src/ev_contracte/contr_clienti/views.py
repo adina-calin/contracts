267ca6d75ab8ae269b39055eba4371f74b7bf9ec
adina-calin <adyna.calin@gmail.com>
2020-07-07 11:51:52 +0300

    creare initiala views si urls

--- src/ev_contracte/contr_clienti/views.py
+++ src/ev_contracte/contr_clienti/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse('acasa')
+
+def contract(request):
+    return HttpResponse('contract')
+
+def actaditional(request):
+    return HttpResponse('actaditional')
+
--- src/ev_contracte/ev_contracte/urls.py
+++ src/ev_contracte/ev_contracte/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contracte/', include('contr_clienti.urls'))
 ]
 
 
