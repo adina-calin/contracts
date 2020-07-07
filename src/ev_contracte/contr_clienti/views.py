dcd76b3c4a8a52f85fae9afd25eadaca594c994f
adina-calin <adyna.calin@gmail.com>
2020-07-07 16:39:30 +0300

    afisare lista contracte si detaliile contr select

--- src/ev_contracte/contr_clienti/views.py
+++ src/ev_contracte/contr_clienti/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 
 def home(request):
-    return render(request, ('contr_clienti/home.html'))
+    contracte = Contract.objects.all()
+    total_contracte = contracte.count()
+    context = {'contracte': contracte, 'total_contracte': total_contracte}
+    return render(request, ('contr_clienti/home.html'), context)
 
-def contract_detalii(request):
-    return render(request, ('contr_clienti/contract_detail.html'))
+def contract_detalii(request, pk1):
+    contract = Contract.objects.get(id=pk1)
+    acteaditionale = contract.actaditional_set.all()
+    aplicatii = contract.aplicatii.all()
+    produse = contract.produse.all()
+    servicii = contract.servicii.all()
+
+    context = {
+        'contract': contract, 
+        'acteaditionale':acteaditionale, 
+        'aplicatii':aplicatii, 
+        'produse': produse, 
+        'servicii':servicii,
+        }
+    return render(request, ('contr_clienti/contract_detail.html'), context)
 
 def actaditional_detalii(request):
     return render(request, ('contr_clienti/actaditional_detail.html'))
