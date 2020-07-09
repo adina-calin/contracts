e56144de5bebd5558c54b34fb0801fc862db8e0b
adina-calin <adyna.calin@gmail.com>
2020-07-09 15:31:50 +0300

    media_url este media

--- src/ev_contracte/ev_contracte/settings.py
+++ src/ev_contracte/ev_contracte/settings.py
@@ -129,13 +129,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
