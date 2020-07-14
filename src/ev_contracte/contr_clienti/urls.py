9fb9041585fa3c84f0c0d12413be375471fef310
adina-calin <adyna.calin@gmail.com>
2020-07-14 09:10:35 +0300

    comment: url-ul catre upload. Template html

--- src/ev_contracte/contr_clienti/urls.py
+++ src/ev_contracte/contr_clienti/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='acasa'),
     path('contract/<int:pk1>/', views.contract_detalii, name='contract-detail'),
+    path('contract/<int:pk1>/upload', views.contract_pdf, name='contract-upload'),
     path('contract/<int:pk1>/actaditional/<int:pk2>/', views.actaditional_detalii, name='actaditional-detail'),
     path('contract/new/', views.creeaza_contract, name='contract-nou'),
     path('contract/<int:pk1>/actaditional/new/', views.creeaza_actaditional, name='actaditional-nou'),
