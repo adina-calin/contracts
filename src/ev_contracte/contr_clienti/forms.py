3668edcb8e15338be986123bf45ad8f5561facb7
adina-calin <adyna.calin@gmail.com>
2020-08-20 10:31:02 +0300

    add, modifica contract, aa - modificare formular

--- src/ev_contracte/contr_clienti/forms.py
+++ src/ev_contracte/contr_clienti/forms.py
@@ -11,19 +11,46 @@ class ActAditionalForm(forms.ModelForm):
         model = ActAditional
         fields = ('nr_registru', 'contract', 'nr_actaditional', 'data_actaditional', 'data_incepere_actaditional', 'data_sfarsit_actaditional', 'produse', 'servicii', 'aplicatii', 'observatii')
 
-        # widgets = {
-        #     'nr_registru': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'contract': forms.Select(attrs={'class': 'form-control'}),
-        #     'nr_actaditional': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'data_actaditional': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'data_incepere_actaditional': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'data_sfarsit_actaditional': forms.TextInput(attrs={'class': 'form-control'}),
-        #     'produse': forms.Select(attrs={'class': 'form-control'}),
-        #     'servicii': forms.Select(attrs={'class': 'form-control'}),
-        #     'aplicatii': forms.Select(attrs={'class': 'form-control'}),
-        #     'observatii': forms.Textarea(attrs={'class': 'form-control'}),
-        # }
+        labels = {
+            'nr_registru': 'Numar registru',
+            'nr_actaditional': 'număr',
+            'data_actaditional': 'data',
+            'data_incepere_actaditional': 'Actul adițional începe în:',
+            'data_sfarsit_actaditional': 'și se încheie în:'
+        }
 
+        help_texts = {
+            'data_incepere_actaditional': '*Format data: ll/zz/aaaa.',
+            'data_sfarsit_actaditional': '*Format data: ll/zz/aaaa.',
+            'data_actaditional': '*Format data: ll/zz/aaaa.',
+            'produse': '*Selectați produsele aferente actului adițional',
+            'servicii': '*Selectați produsele aferente actului adițional',
+            'aplicatii': '*Selectați produsele aferente actului adițional',
+            'observatii': '*Introduceti detalii suplimentare despre actul adițional'
+        }
+
+        widgets = {
+            'nr_registru': forms.NumberInput(),
+            'contract': forms.Select(attrs={'class': 'form-control'}),
+            'nr_actaditional': forms.NumberInput(),
+            'data_actaditional': DatePickerInput(),
+            'data_incepere_actaditional': DatePickerInput(),
+            'data_sfarsit_actaditional': DatePickerInput(),
+            'produse': forms.CheckboxSelectMultiple(attrs={'class': 'form-control', 'size': 3}),
+            'servicii': forms.CheckboxSelectMultiple(attrs={'class': 'form-control', 'size': 3}),
+            'aplicatii': forms.CheckboxSelectMultiple(attrs={'class': 'form-control', 'size': 3}),
+            'observatii': forms.Textarea(attrs={'class': 'form-control', 'rows': 2}),
+        }
+
+    def clean_data_sfarsit_aa(self):
+        data_incepere_actaditional = self.cleaned_data.get('data_incepere_actaditional')
+        data_sfarsit_actaditional = self.cleaned_data.get('data_sfarsit_actaditional')
+
+        if data_incepere_actaditional and data_sfarsit_actaditional:
+            if data_incepere_actaditional > data_sfarsit_actaditional:
+                raise forms.ValidationError("Data este anterioara datei de inceput!")
+            return data_sfarsit_actaditional
+            
 
 class ContractUForm(forms.ModelForm):
     class Meta():
